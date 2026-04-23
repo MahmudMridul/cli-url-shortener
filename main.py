1fec0abbf9b549cde53362c8eb1bed83a88b4393
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2026-04-23 12:08:56 +0600

    implement validations for url and shortened url

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import requests
+
+MIN_SHORT_URL_LEN = 5
+
+def url_is_valid(url: str):
+    try:
+        response = requests.head(url, timeout=5)
+        return response.status_code < 400
+    except requests.RequestException:
+        return False
+
+def shortened_url_is_valid(url: str):
+    stripped = url.strip()
+    if len(stripped) < MIN_SHORT_URL_LEN:
+        print("Enter at least 5 characters")
+        return False
+    return True
+
 def main():
-    print("Hello from cli-url-shortener!")
+    url = input("Enter the url:\n")
+    if url_is_valid(url):
+        shortened_url = input("Enter preferred shortened url:\n")
+
+        if shortened_url_is_valid(shortened_url):
+            print(shortened_url)
+    else:
+        print("The given URL is not valid or doesn't exist")
 
 
 if __name__ == "__main__":
